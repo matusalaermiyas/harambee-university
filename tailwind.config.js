d98e9746b709e6beb8fc005f01c3df0b35888e6f
MatusalaErmiyas <matusala.dev@gmail.com>
2024-11-08 16:34:28 +0300

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        customFont: ["Manrope"],
+      },
+      colors: {
+        primaryColor: "#f9a825",
+        wrapperBorderColor: "#E9E9E9",
+        inputBorderColor: "#E5E7EB",
+        textInputColor: "#6B7280",
+      },
+    },
   },
   plugins: [],
-}
-
+};
